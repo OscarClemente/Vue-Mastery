54d6c6101b3e84c8cc39bc79127d5cdbda87d685
OscarClemente <oscar.clemente@gmail.com>
2021-02-01 18:49:37 +0100

    Fixed nested repo

---
+++ Intro-to-Vue-3/components/ReviewList.js
@@ -0,0 +1,34 @@
+app.component('review-list', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `
+    <div class="review-container">
+        <h3>Reviews:</h3>
+        <ul>
+            <li v-for="(review, index) in reviews" :key="index">
+                {{ review.name }} gave this {{ review.rating }} stars
+                
+                <br/>
+                "{{ review.review }}"
+                <br/>
+                Recommended: {{ isRecommended(index) }}
+            </li>
+        </ul>
+    </div>
+    `,
+    methods: {
+        isRecommended(index) {
+            if (this.reviews[index].recommend)
+            {
+                return 'Yes'
+            }
+            return 'No'
+        }
+    }
+})
